1a3f4a6587613650d25ac62a408cc1fb077111e1
stepheljobs <stephelgeorgemaca@gmail.com>
2015-12-14 09:56:09 +0800

    Multiple uploads

--- route/upload.js
+++ route/upload.js
@@ -9,12 +9,22 @@ var client = new S3FS('bucket-name/bucket-folder-optional', {
 
 exports.goUpload = function(req, res) {
   // client.create();
-  var file = req.files.fileUpload;
-  var stream = fs.createReadStream(file.path);
-  return client.writeFile(file.originalFilename, stream).then( function() {
-     fs.unlink(file.path, function(err){
-       if (err) throw err;
-     });
-     res.send(file.originalFilename);
+  console.log('------> start uploading');
+  var files = req.files.fileUpload;
+  files.forEach(function (file, index) {
+      console.log('##############');
+      console.log(file);
+      console.log(index);
+      var stream = fs.createReadStream(file.path);
+      return client.writeFile(file.originalFilename, stream).then( function(etag) {
+         fs.unlink(file.path, function(err){
+           if (err) throw err;
+         });
+        //  https://ipostmo-s3-staging.s3.amazonaws.com/products/testimage1.jpg
+        //  res.send(file.originalFilename);
+        console.log('---> value: ', etag); //
+        console.log('uploaded success: ', index);
+      });
   });
+  console.log('------> end here');
 };
